a6f290bfea0e61899caedd12d9ec469a2a456c93
hello-cjlee-s-world <153203021+hello-cjlee-s-world@users.noreply.github.com>
2024-03-01 00:00:43 +0900

    3D 게임 중간 커밋
    1. 에셋 스토어에서 Standard Assets다운
    - 존재하지 않음. 어느순간부터 제공하지 않음
    - https://github.com/RoadwayVR/UnityStandardAssets 에서 다운받아서 프로젝트 Assets 디렉토리에 붙여넣기해서 임포트 시킴. 해결
    - 오브젝트 색이 전부 핑크색으로 나오길래 구글링해보니 해당 오브젝트에 설정된 Material 찾아서 Shader를 standard로 바꿔주라고 함. 해결
    
    2. 바닥 생성
    - Standard Assets
    
    3. 캐릭터 생성
    - Standard Assets
    
    4. 캐릭터 따라가는 카메라 설정
    - 기본 main camera 삭제
    - Standard Assets 의 MultipurposeCameraRig 설정.
    - 3의 캐릭터 tag에 Player 설정하면 해당 태그의 오브젝트 따라가는 카메라 설정 완료
    5. 계단, 길, 집, 결승점 생성
    
    6. 박스 생성
    
    - 박스는 또 1의 링크에서 제공하는 데이터가 없길래 이 책의 저자가 제공하는 프로젝트 소스에서 붙여넣기
    7. 계단, 길, 집, 결승점의 텍스쳐 변경
    - Asset Store 에서 Yughues Free Architectural Materials(무료) 임포트하여 사용
    - 임포트 후 원하는 텍스쳐를 적용할 오브젝트에 어태치 하면 적용 완료
    
    8. 라이트 추가
    - 하이어라키 창에서 create -> light 에서 생성 가능
    - p287
    
    9. 낙하 판정 영역 생성
    - 3D 오브젝트인 cube 생성 후 장판 하단에 영역을 넓게 펼침
    - Inspector 창에서 Mesh Renderer를 체크 해제 또는 삭제하면 안보이게 설정 가능
    - Inspector의 Box Collider 에서 Is Trigger를 체크하면 기존 충돌 판정이 사라지고 임의의 충돌판정을 script에서 처리 가능
    
    10. 낙하 판정 스크립트 생성
    - p304 참고
    
    11. 결승점 생성
    - 마찬가지로 3D cube 생성 후 Mesh Renderer, Box Collider 설정
    
    12. 골인 스크립트 생성
    - p312 참고
    
    13. 타이머 생성
    - UI의 Text로 생성
    
    14. 타이머 스크립트 작성
    - p320 참고
    
    15. 결과 화면 생성
    - UI의 Text로 생성
    
    16. 재시작 버튼 생성
    - UI의 button으로 생성
    
    17. 결과 화면 숨기기
    - empty  생성 후 결과 화면 텍스트들 자식으로 넣은 후 empty 오브젝트 옆에 체크 해제
    
    18. 결과 화면 처리 스크립트 생성
    - p335 참고
    - canvas에 스크립트 어태치

---
+++ StageRun/Assets/Scripts/Goal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour{
+    public static bool goal;
+
+    void Start(){
+        goal = false;
+    }
+    void OnTriggerEnter(Collider col){
+        if(col.gameObject.tag == "Player"){
+            goal = true;
+        }
+    }
+}
