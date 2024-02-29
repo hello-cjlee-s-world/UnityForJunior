665322204da18db2edb61bb6eed99b250bc5383a
hello-cjlee-s-world <153203021+hello-cjlee-s-world@users.noreply.github.com>
2024-02-29 20:08:40 +0900

    ChickAttack UI 완성
    p186 ~ p242
    1. 프로젝트 export/import 실습
    2. 새로운 신 생성
    - 신(Scene)들은 파일로 저장되며 하이어라키에 개별적으로 뜨지 않고 Assets에 뜬다.
    3. 타이틀 이미지 배치
    - UI -> Image 생성
    - Image 오브젝트 인스펙터에 타이틀 이미지 드래그 앤 드롭
    - Anchor 상단 중앙 고정
    4. 시작 버튼 배치
    - 버튼 색, 마우스 올렸을때 바뀌는 색 등 실습
    - Anchor 상단 중앙 고정
    5. 배경 색 변경
    - UI -> Panel 으로 배경 생성
    - 하이어라키 상단으로 옮겨 화면상 제일 뒤에 위치하도록 조정
    6. 게임 시작 스크립트 생성
    - using Unityegine.SceneManagement 라이브러리 임포트해줘야함
    - SceneManager.LoadScene("신 이름") 으로 신 변경
    7. 버튼에 스크립트 적용
    - 버튼 오브젝트에 스크립트 어태치하여 적용하기
    - button 컴포넌트의 On Click() 에서 버튼 이름 찾아서 고르고 함수 설정
    8. Build setting 변경
    - 기존 SampleScene 제거 후 변경할 신들 드래그앤 드랍 후 창 닫기
    - 위에서부터 순서대로 신이 이동하므로 순서 맞춰주기

---
+++ ChickUI/Assets/script/ChickGenerator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickGenerator : MonoBehaviour{
+    public GameObject obj;
+    public float interval = 3.0f;
+
+    void Start(){
+        InvokeRepeating("SpawnObj", 0.1f, interval);
+    }
+
+    void SpawnObj(){
+        Instantiate(obj, transform.position, transform.rotation);
+    }
+}
---
+++ ChickUI/Assets/script/DestoryObj.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryObj : MonoBehaviour
+{
+    public float deleteTime = 2.0f;
+    // Start is called before the first frame update
+    void Start(){
+        Destroy(gameObject, deleteTime);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
